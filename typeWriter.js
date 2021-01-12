218a28c42b52bb63be1cb3a918e18467f063a61b
QuintenAiton <quinaiton@hotmail.com>
2021-01-12 22:02:57 +0000

    ran module through eslint for formatting

--- typeWriter.js
+++ typeWriter.js
@@ -1,13 +1,13 @@
 const typeWriter = (string) => {
-  let timeout = 0
+  let timeout = 0;
   for (const letter of string) {
     setTimeout(() => {
-      process.stdout.write(letter)
-    }, timeout += 50)
+      process.stdout.write(letter);
+    }, timeout += 50);
   }
   setTimeout(() => {
-    process.stdout.write('\n')
+    process.stdout.write('\n');
   }, string.length * 50);
-}
+};
 
-console.log(typeWriter('hello there from lighthouse labs'))
+console.log(typeWriter('hello there from lighthouse labs'));
